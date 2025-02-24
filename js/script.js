bfe4d93cb5298e45de1e9159a2b71547b9340243
Promise278 <promiseobi2007@gmail.com>
2025-02-24 10:44:23 +0100

    added image

--- js/script.js
+++ js/script.js
@@ -1,36 +1,3 @@
-// let menu = document.getElementById("nav-menu");
-// let btnOpen = document.getElementById("menu-open");
-// let btnClose = document.getElementById("menu-close");
-
-// [btnOpen, btnClose].forEach(btn => btn.onclick = () => {
-//     menu.classList.toggle("hidden");
-//     btnOpen.classList.toggle("hidden");
-//     btnClose.classList.toggle("hidden");
-// });
-
-// document.addEventListener("DOMContentLoaded", function() {
-//     let menu = document.getElementById("nav-menu");
-//     let btnOpen = document.getElementById("menu-open");
-//     let btnClose = document.getElementById("menu-close");
-
-//     [btnOpen, btnClose].forEach(btn => btn.addEventListener("click", () => {
-//         menu.classList.toggle("hidden");
-//         btnOpen.classList.toggle("hidden");
-//         btnClose.classList.toggle("hidden");
-//     }));
-// });
-
-
-// document.addEventListener("DOMContentLoaded", function () {
-//     let menu = document.getElementById("nav-menu");
-//     let menuToggle = document.getElementById("menu-open");
-
-//     menuToggle.addEventListener("click", function () {
-//         menu.classList.toggle("hidden"); // Show/hide menu
-//         this.name = this.name === "menu" ? "close" : "menu"; // Toggle icon
-//     });
-// });
-
 let menuButton = document.getElementById("menu-open");
 
 menuButton.onclick = () => {
